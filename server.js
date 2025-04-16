c8d7a2066cd7fc0c592418160cedc604a34ae9bc
Ydavidovici <150648286+Ydavidovici@users.noreply.github.com>
2025-04-16 15:48:27 -0400

    updated font, added js script

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/pages/about.html'))
+});
+
+app.get('/services', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/pages/services.html'))
+});
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
